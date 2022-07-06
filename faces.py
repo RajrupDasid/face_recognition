effd9d9576d57b7cf2f2c4bcf47371f3d9abac20
RajrupDasid <rajrupdasid@outlook.com>
2022-07-06 18:07:37 +0530

    recognizer bild up and image live label added

--- faces.py
+++ faces.py
@@ -1,8 +1,14 @@
 import numpy as np
 import cv2
+import pickle
 
+recognizer=cv2.face.LBPHFaceRecognizer_create()
 face_cascade=cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
-
+recognizer.read("recognizer.yml")
+labels={}
+with open("labels.pickle","rb") as f:
+    og_labels=pickle.load(f)
+    labels={v:k for k,v in og_labels.items()}
 
 cap=cv2.VideoCapture(0)
 while True:
@@ -14,6 +20,17 @@ while True:
         print(x,y,w,h)
         roi_gray=gray[y:y+h, x:x+w] #regeion of interest of gray scale
         roi_color=frame[y:y+h, x:x+w] # regeion of interest for color scale
+        # recognizer 
+        id_, conf=recognizer.predict(roi_gray)
+        if conf >=45 and conf <= 85:
+            print(id_)
+            print(labels[id_])
+            font=cv2.FONT_HERSHEY_SIMPLEX
+            name=labels[id_]
+            color=(255,255,255)
+            stroke=2
+            cv2.putText(frame,name,(x,y),font,1,color,stroke,cv2.LINE_AA)
+        #extras
         img_item="people-face.png"
         cv2.imwrite(img_item,roi_gray)
         # square up around faces 
